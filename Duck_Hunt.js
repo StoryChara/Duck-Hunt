2d135ef1c531a0318eeec34e66b8c204b9bb5852
StoryChara <mjarah@unal.edu.co>
2025-03-03 19:54:50 -0500

    subir recursos + html

--- Duck_Hunt.js
+++ Duck_Hunt.js
@@ -1,8 +1,15 @@
-function setup() {
 
+
+function preload() {
+  
+}
+
+function setup() {
+  const canvas = createCanvas(800, 450);
+  canvas.parent('canvas-container');
 }
 
 
 function draw() {
-
+  background(0);
 }
